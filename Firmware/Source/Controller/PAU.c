6e54ab37958e1a9a97ef54c8a4ce673148221d84
d.ivanov <d.ivanov@proton-electrotex.com>
2022-11-08 11:31:29 +0300

    Добавлена библиотека PAU

---
+++ Firmware/Source/Controller/PAU.c
@@ -0,0 +1,52 @@
+// Header
+//
+#include "PAU.h"
+
+// Includes
+//
+#include "DataTable.h"
+#include "DeviceObjectDictionary.h"
+#include "BCCIMHighLevel.h"
+
+
+// Functions
+//
+bool PAU_UpdateState(Int16U* Register)
+{
+	return (DataTable[REG_PAU_EMULATED]) ? true : BHL_ReadRegister(DataTable[REG_PAU_CAN_ID], REG_PAU_DEV_STATE, Register);
+}
+//--------------------------------------
+
+bool PAU_Configure(Int16U Channel, float Range, float Time)
+{
+	if(DataTable[REG_PAU_EMULATED])
+		return true;
+
+	if(BHL_WriteRegister(DataTable[REG_PAU_CAN_ID], REG_PAU_CHANNEL, Channel))
+		if(BHL_WriteRegisterFloat(DataTable[REG_PAU_CAN_ID], REG_PAU_RANGE, Range))
+			if(BHL_WriteRegisterFloat(DataTable[REG_PAU_CAN_ID], REG_PAU_MEASUREMENT_TIME, Time))
+				BHL_Call(DataTable[REG_PAU_CAN_ID], ACT_PAU_PULSE_CONFIG);
+					return true;
+
+	return false;
+}
+//--------------------------------------
+
+bool PAU_ClearFault()
+{
+	return (DataTable[REG_PAU_EMULATED]) ? true : BHL_Call(DataTable[REG_PAU_CAN_ID], ACT_PAU_FAULT_CLEAR);
+}
+//--------------------------------------
+
+bool PAU_ClearWarning()
+{
+	return (DataTable[REG_PAU_EMULATED]) ? true : BHL_Call(DataTable[REG_PAU_CAN_ID], ACT_PAU_WARNING_CLEAR);
+}
+//--------------------------------------
+
+bool PAU_ReadMeasuredData(float* Data)
+{
+	return (DataTable[REG_PAU_EMULATED]) ? true : BHL_ReadRegisterFloat(DataTable[REG_PAU_CAN_ID], REG_PAU_RESULT_CURRENT, Data);
+}
+//--------------------------------------
+
---
+++ Firmware/Source/Controller/PAU.h
@@ -0,0 +1,35 @@
+#ifndef CONTROLLER_PAU_H_
+#define CONTROLLER_PAU_H_
+
+// Includes
+//
+#include "stdinc.h"
+
+// Definitions
+//
+#define PAU_CHANNEL_LCTU			1
+#define PAU_CHANNEL_IGTU			2
+//
+#define PAU_CONFIG_TIMEOUT			1000
+
+// Types
+//
+typedef enum __PAUState
+{
+	PS_None = 0,
+	PS_Fault = 1,
+	PS_Disabled = 2,
+	PS_Ready = 3,
+	PS_InProcess = 4,
+	PS_ConfigReady = 5
+} PAUState;
+
+// Functions
+//
+bool PAU_UpdateState(Int16U* Register);
+bool PAU_Configure(Int16U Channel, float Range, float Time);
+bool PAU_ClearFault();
+bool PAU_ClearWarning();
+bool PAU_ReadMeasuredData(float* Data);
+
+#endif /* CONTROLLER_PAU_H_ */
